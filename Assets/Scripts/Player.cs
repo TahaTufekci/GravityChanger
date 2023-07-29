16bb1cedc4bc676ad265f2e54b046190e5f760b4
Yasemin Toraman <yaseminntoraman@gmail.com>
2023-07-29 21:11:55 +0300

    update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,7 +11,8 @@ public class Player : MonoBehaviour
     public bool stopMovement;
     private Rigidbody2D playerRb;
     private Vector2 stageDimensions;
-    int originalXRotation;
+    int originalXRotation, originalYRotation;
+    
 
     GManager gameManager;
 
@@ -26,36 +27,40 @@ public class Player : MonoBehaviour
     {
         stageDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height,0));
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            playerRb.gravityScale *= -1;
-            if (playerRb.gravityScale > 0)
-            {
-                transform.eulerAngles = new Vector3 ((0) , transform.eulerAngles.y, 0);
-                originalXRotation = 0;
-            }
-            else
-            {
-                transform.eulerAngles = new Vector3 ((180) , transform.eulerAngles.y, 0);
-                originalXRotation = 180;
-            }
-        }
+
         if (!stopMovement)
         {
             transform.Translate(transform.right * speed * direction * Time.deltaTime);
             if (transform.position.x >= stageDimensions.x) //go to left if at the right edge of screen
             {
                 transform.eulerAngles = new Vector3 (originalXRotation, -180, 0);
+                originalYRotation = -180;
                 direction = -1;
             }
             else if (transform.position.x <= -stageDimensions.x) //go to right if at the left edge of screen
             {
                 transform.eulerAngles = new Vector3 (originalXRotation, 0, 0);
+                originalYRotation = 0;
                 direction = 1;
             }
 
         }
-        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            playerRb.gravityScale *= -1;
+            if (playerRb.gravityScale > 0)
+            {
+                transform.eulerAngles = new Vector3((0), originalYRotation, 0);
+                originalXRotation = 0;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3((180), originalYRotation, 0);
+                originalXRotation = 180;
+            }
+        }
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
