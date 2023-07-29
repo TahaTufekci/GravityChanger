5bd74a7ce483e84e754a728b2a8ec37f275e8ed3
Yasemin Toraman <yaseminntoraman@gmail.com>
2023-07-29 21:17:52 +0300

    audio manager added

---
+++ Assets/Scripts/SoundScripts/AudioManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Audio[] audios;
+    public static AudioManager instance;
+
+    private void Start()
+    {
+        playSound("Background");
+        if (instance == null)
+            instance = this;
+
+        else
+            Destroy(this.gameObject);
+
+        GameObject.DontDestroyOnLoad(instance);
+
+    }
+
+    public void playSound(string name)
+    {
+        foreach (Audio item in audios)
+        {
+            if (item.name == name)
+            {
+                if (item.source == null)
+                    item.source = gameObject.AddComponent<AudioSource>();
+                item.source.clip = item.clip;
+                item.source.volume = item.volume;
+                item.source.loop = item.loop;
+
+                item.source.Play();
+                break;
+
+            }
+        }
+    }
+}
