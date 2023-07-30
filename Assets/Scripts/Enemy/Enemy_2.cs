939c415dd5aefaf163a0c7d218d6d7cf0b6921d9
Yasemin Toraman <yaseminntoraman@gmail.com>
2023-07-30 13:22:28 +0300

    update

--- Assets/Scripts/Enemy/Enemy_2.cs
+++ Assets/Scripts/Enemy/Enemy_2.cs
@@ -9,7 +9,9 @@ public class Enemy_2 : Enemy
     [SerializeField] float speed; //movement speed
     private Vector3 startPosition;
     private bool moveRight = true;
-    
+
+    public int direction; //direction of movement, left or righ (1 or -1)
+
     void Start()
     {
         startPosition = transform.position;
@@ -17,17 +19,24 @@ public class Enemy_2 : Enemy
     public override void EnemyMovement()
     {
         Vector3 stageDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height,0));
+
         if (transform.position.x >= stageDimensions.x) //go to left if at the right edge of screen
         {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+            direction *= -1;
             moveRight = false;
         }
         else if (transform.position.x <= -stageDimensions.x) //go to right if at the left edge of screen
         {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            direction *= -1;
             moveRight = true;
         }
+
+
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         EnemyMovement();
         if (moveRight)
@@ -41,12 +50,12 @@ public class Enemy_2 : Enemy
     }
     void MoveRight()
     {
-        startPosition += transform.right * Time.deltaTime * speed;
+        startPosition += transform.right * Time.deltaTime * speed * direction;
         transform.position = startPosition + transform.up * Mathf.Sin(Time.time * speed) * 0.5f;
     }
     void MoveLeft()
     {
-        startPosition -= transform.right * Time.deltaTime * speed;
+        startPosition -= transform.right * Time.deltaTime * speed * direction;
         transform.position = startPosition + transform.up * Mathf.Sin(Time.time * speed) * 0.5f;
     }
 }
